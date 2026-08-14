1f450e72d39acf9211aeb0a37fe70f12d47bb717
tianmei-dev <zhangtianmei69@gmail.com>
2026-08-14 19:10:24 +0800

    应用名称&图标替换

--- app/src/main/java/com/aurora/calculatorvault/feature/privatemedia/domain/SystemMediaRestoreManager.kt
+++ app/src/main/java/com/aurora/calculatorvault/feature/privatemedia/domain/SystemMediaRestoreManager.kt
@@ -160,8 +160,8 @@ class SystemMediaRestoreManager(
 
     private fun relativePath(mediaType: VaultMediaType): String =
         when (mediaType) {
-            VaultMediaType.IMAGE -> "${Environment.DIRECTORY_PICTURES}/CalculatorVault/"
-            VaultMediaType.VIDEO -> "${Environment.DIRECTORY_MOVIES}/CalculatorVault/"
+            VaultMediaType.IMAGE -> "${Environment.DIRECTORY_PICTURES}/应用加密计算器/"
+            VaultMediaType.VIDEO -> "${Environment.DIRECTORY_MOVIES}/应用加密计算器/"
         }
 
     private fun safeMimeType(request: SystemMediaRestoreRequest): String =
